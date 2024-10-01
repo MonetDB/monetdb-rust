ed089da956ca82f772c5233c0b68f42f26f93029
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2024-10-01 15:36:14 +0200

    Get rid of MonetKind, we're not using it

--- src/monettypes.rs
+++ src/monettypes.rs
@@ -7,18 +7,6 @@
 // Copyright 2024 MonetDB Foundation
 use std::fmt;
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-pub enum MonetKind {
-    /// A common integer, that is, up to i64.
-    Integer,
-    /// Huge integers are sometimes treated differently
-    HugeInteger,
-    /// Decimals have precision and scale and need to be extracted differently
-    Decimal,
-    /// UTF-8 encoded text without NUL bytes
-    Text,
-}
-
 pub type Precision = u8;
 
 pub type Scale = u8;
@@ -56,15 +44,6 @@ impl fmt::Display for MonetType {
 }
 
 impl MonetType {
-    pub fn kind(&self) -> MonetKind {
-        use MonetType::*;
-        match self {
-            Bool | TinyInt | SmallInt | Int | BigInt | Oid => MonetKind::Integer,
-            HugeInt => MonetKind::HugeInteger,
-            Decimal(_, _) => MonetKind::Decimal,
-            Varchar(_) => MonetKind::Text,
-        }
-    }
 
     pub fn prototype(code: &str) -> Option<Self> {
         use MonetType::*;
